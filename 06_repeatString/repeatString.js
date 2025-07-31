0219c65e49e877483119216c4c41246e79d5e536
Pourush-Tyagi <Pourush.61947@gmail.com>
2025-07-31 12:48:47 +0530

    updated the repeatString.js with logic and the repeatString.spec.js for running all the tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text,times) {
+    if(times < 0){
+      return 'ERROR';
+    }else {
+        let result='';
+        for(let i=0; i<times; i++){
+            result= result+text;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
