8d714bd9126531d142f3dbdd6dac9710c16df132
Pourush-Tyagi <Pourush.61947@gmail.com>
2025-08-02 00:51:51 +0530

    updated the calculator.js with logic and the calculator.spec.js for running all the tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,54 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let x = 0;
+  for(i of arr){
+    x = x + i;
+  }
+  return x;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  	let x = 1;
+  for(i of arr){
+    x = x * i;
+  }
+  return x;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let x = b;
+  let y = 1;
+  while(x>0){
+    y = y*a;
+    --x;
+  }
+  return y;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let x = a;
+  let y = 1;
+  if(a===0){
+    return 1;
+  }
+  else if(a<=0){
+    return 'undefined'
+  }else if(a>0){
+
+    while(x>0){
+    y = y*x;
+    --x;
+    }
+    return y;
+  }
+  
 	
 };
 
