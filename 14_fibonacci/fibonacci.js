28a14c81b54f7a402f1a0d714a9a399922e35410
Pourush-Tyagi <Pourush.61947@gmail.com>
2025-08-02 01:37:42 +0530

    updated the fibonacci.js with logic and the fibonacci.spec.js for running all the tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  index = Number(index);
+  if (index < 0) return "OOPS";
+  if (index === 0) return 0;
 
+  let arr = [0, 1];
+  for (let i = 2; i <= index; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr[index];
 };
 
 // Do not edit below this line
