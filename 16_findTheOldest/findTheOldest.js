b5315980a88754ac2fd35c9c7a65536d7c202942
Pourush-Tyagi <Pourush.61947@gmail.com>
2025-08-02 01:58:37 +0530

    updated the findtheoldest.js with logic and the findtheoldest.spec.js for running all the tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    const currentYear = new Date().getFullYear();
 
+    const oldest = people.reduce((oldest, person) => {
+    const age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+    return age > oldestAge ? person : oldest;
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
