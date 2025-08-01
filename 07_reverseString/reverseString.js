c2716eacae125701e4f425acd91345dbbae4d524
Pourush-Tyagi <Pourush.61947@gmail.com>
2025-08-01 11:06:38 +0530

    updated the reverseString.js with logic and the reverseString.spec.js for running all the tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    const arr = str.split('');
+    for (let i = 0; i < arr.length / 2; i++) {
+        const j = arr.length - 1 - i;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
